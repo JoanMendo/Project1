fd7a2991d7dec82837907e97cfe8ef264d05d2bd
Joan Mendo <joan.mendo.7e7@itb.cat>
2024-11-15 15:55:35 +0100

    Añadiendo cambio de nivel

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,30 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class CharacterMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+
     public float speed = 5f;
     public GameObject respawnPosition;
     private Vector2 movement;
     private float inputX;
     private float inputY;
-    //private GameObject originalTrail;
-    public string trailName;
-   // private GameObject temporalTrail;
-    //private GameObject temporalBackTrail;
-    private Queue queue;
-    private Vector2 lastOffset;
+    private static CharacterMovement instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+       
+    }
+
 
     void Start()
     {
-        queue = new Queue();
+
         rb = GetComponent<Rigidbody2D>();
-       /* originalTrail = GameObject.Find(trailName);
-        originalTrail.transform.position = transform.position;
-        CreateTrail();*/
+
         
+
+
     }
 
     // Update is called once per frame
@@ -34,51 +46,17 @@ public class CharacterMovement : MonoBehaviour
         inputY = Input.GetAxisRaw("Vertical");
         movement = new Vector2(inputX, inputY).normalized;
         rb.velocity = movement * speed;
-        Vector2 offset = movement * 0.2f;
-        if (offset != Vector2.zero)
-        {
-            lastOffset = offset;
-        }
-        
 
-       /* temporalTrail.transform.position = transform.position;
-        temporalBackTrail.transform.position = (Vector2)transform.position + lastOffset; */
+
     }
 
-    /*
-    public void CreateTrail()
-    {
-        if (queue.Count >= 10)
-        {
-            GameObject trail = (GameObject)queue.Dequeue();
-            GameObject backTrail = (GameObject)queue.Dequeue();
-            Destroy(trail);
-            Destroy(backTrail);
-
-        }
-        
-        temporalTrail = Instantiate(originalTrail);
-        temporalTrail.name = "Mondongo";
-        originalTrail = temporalTrail;
-        temporalBackTrail = Instantiate(originalTrail);
-        temporalBackTrail.name = "Mondongo";
-        temporalBackTrail.GetComponent<TrailRenderer>().startColor = Color.black;
-        temporalBackTrail.GetComponent<TrailRenderer>().endColor = Color.black;
-        temporalBackTrail.GetComponent<TrailRenderer>().widthMultiplier *= 1.15f;
-        temporalBackTrail.GetComponent<TrailRenderer>().sortingOrder = 4;
-        temporalTrail.GetComponent<TrailRenderer>().emitting = true;
-        temporalBackTrail.GetComponent<TrailRenderer>().emitting = true;
-        queue.Enqueue(temporalTrail);
-        queue.Enqueue(temporalBackTrail);
-
-        
-    } */
 
     public void Die()
     {
-        /*CreateTrail();
+
         StartCoroutine(respawnFreeze());
-        transform.position = respawnPosition.transform.position;*/
+        transform.position = respawnPosition.transform.position;
+
 
     }
 
@@ -86,7 +64,7 @@ public class CharacterMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Terrain")
         {
-            Die();
+            //Die();
         }
     }
 
@@ -97,6 +75,15 @@ public class CharacterMovement : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         rb.constraints = RigidbodyConstraints2D.None;
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+
+
+
+
+    public void OnSceneLoaded()
+    {
+        respawnPosition = GameObject.FindGameObjectWithTag("Respawn");
     }
 
 }
---
+++ Assets/Scripts/LevelChange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    public GameObject player;
+
+    public void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Personaje");
+    }
+
+
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+            
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        player.GetComponent<CharacterMovement>().Die();
+
+
+    }
+}
