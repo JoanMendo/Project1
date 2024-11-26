c4d06944bbc566a2918345ef8be823cafc67cfed
Joan Mendo <joan.mendo.7e7@itb.cat>
2024-11-26 14:54:12 +0100

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/LevelChange.cs
+++ Assets/Scripts/LevelChange.cs
@@ -10,6 +10,9 @@ public class LevelChange : MonoBehaviour
     public Canvas playerInterface;
     public int level;
     public int exit;
+    public static int Protanopia = 0;
+    public static int Deuteranopia = 0;
+    public static int Tritanopia = 0;
 
     public void Start()
     {
@@ -19,18 +22,16 @@ public class LevelChange : MonoBehaviour
         UpdateInterface();
     }
 
-    public void UpdateInterface()
-    {
-        Debug.Log("Eyo");
-        playerInterface.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = "Nivel " + level;
-    }
-
     public void OnTriggerEnter2D(Collider2D collision)
     {
         try
         {
             ApiRequest.instance.times[level] = Time.time;
             ApiRequest.instance.exits[level] = exit;
+            if (exit == 0)
+            {
+                checkDaltonism();
+            }
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         catch (Exception e)
@@ -38,8 +39,21 @@ public class LevelChange : MonoBehaviour
             Debug.Log(e.ToString());
             ApiRequest.instance.StartCoroutine(ApiRequest.instance.SendPostRequest());
         }
-        
+    }
 
-
+    public void checkDaltonism()
+    {
+        if (SceneManager.GetActiveScene().name.Contains("Protanopia"))
+        {
+            Protanopia++;
+        }
+        if (SceneManager.GetActiveScene().name.Contains("Deuteranopia"))
+        {
+            Deuteranopia++;
+        }
+        if (SceneManager.GetActiveScene().name.Contains("Tritanopia"))
+        {
+            Tritanopia++;
+        }
     }
 }
